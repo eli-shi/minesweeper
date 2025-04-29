324efee42856ffc8487c8afebb2554b84c100123
eli-shi <shirinyan.elina@gmail.com>
2025-04-29 14:53:36 +0200

    add: useEffect to end game for when the game is won, not lost useEffect to check when all the blocks that are not bombs are revealed, the player is alerted that they won and triggers reset game

--- client/src/hooks/useBoard.tsx
+++ client/src/hooks/useBoard.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { buildInitialBoard } from '../utils/Board';
 import DefaultBlock, { BlockClassName, BlockStatus } from '../utils/Block';
 
 
-export function useBoard({ rows, columns, numberOfMines }: { rows: number, columns: number, numberOfMines: number }) {
+export function useBoard({ rows, columns, numberOfMines, triggerGameOver }: { rows: number, columns: number, numberOfMines: number, triggerGameOver: () => void }) {
 
     const [board, setBoard] = useState(buildInitialBoard(rows, columns, numberOfMines));
 
-    function revealBlock(block: DefaultBlock, triggerGameOver: () => void) {
+    function revealBlock(block: DefaultBlock) {
 
         if (block.blockStatus === BlockStatus.revealed) {
             // If the block is already revealed, do nothing
@@ -29,6 +29,21 @@ export function useBoard({ rows, columns, numberOfMines }: { rows: number, colum
 
 
     }
+
+    useEffect(() => {
+        const revealed = board.reduce((acc, row) => {
+            acc += row.reduce((acc2, block) => {
+                acc2 += (block.blockStatus === BlockStatus.revealed) ? 1 : 0;
+                return acc2;
+            }, 0);
+            return acc;
+        }, 0);
+
+        if (revealed === rows * columns - numberOfMines) {
+            alert("You won!");
+            triggerGameOver();
+        }
+    }, [board]);
     {/* should be callback or useEffect so that it can reset the board whenever a new block is reveal, aka when the block.status changes */ }
 
 
